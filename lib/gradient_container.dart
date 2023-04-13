db6f30e8897bd48159989d1e1cc7401dba19717a
Sergey Klimanoff <iamjsare@gmail.com>
2023-04-12 19:11:41 -0600

    Add stfWidget for rollDice button & add setstate

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 
+import 'dice_roller.dart';
+
 const startAlignment = Alignment.topLeft;
 const endAlignment = Alignment.bottomRight;
 
-class GradientContainer extends StatelessWidget {
+class GradientContainer extends StatefulWidget {
   const GradientContainer(this.color1, this.color2, {super.key});
 
   const GradientContainer.purple({super.key})
@@ -13,41 +15,23 @@ class GradientContainer extends StatelessWidget {
   final Color color1;
   final Color color2;
 
-  void rollDice() {
-    // ...
-  }
+  @override
+  State<GradientContainer> createState() => _GradientContainerState();
+}
 
+class _GradientContainerState extends State<GradientContainer> {
   @override
   Widget build(context) {
     return Container(
       decoration: BoxDecoration(
         gradient: LinearGradient(
-          colors: [color1, color2],
+          colors: [widget.color1, widget.color2],
           begin: startAlignment,
           end: endAlignment,
         ),
       ),
-      child: Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Image.asset(
-              'assets/images/dice-2.png',
-              width: 200,
-            ),
-            const SizedBox(height: 20),
-            TextButton(
-              onPressed: rollDice,
-              style: TextButton.styleFrom(
-                foregroundColor: Colors.white,
-                textStyle: const TextStyle(
-                  fontSize: 28,
-                ),
-              ),
-              child: const Text('Roll Dice'),
-            )
-          ],
-        ),
+      child: const Center(
+        child: DiceRoller(),
       ),
     );
   }
